b0bb03681f5592eb93329c0558a67042c73742c2
Leigh McCulloch <leigh@mcchouse.com>
2016-11-12 05:37:01 +0000

    Add google-cloud-sdk for gsutil
    What
    ===
    Add google-cloud-sdk.
    
    Why
    ===
    So that `gsutil` is installed on the devenv, which is used for pushing
    static websites and files to Google Storage.

--- setup-system/setup.sh
+++ setup-system/setup.sh
@@ -1,3 +1,7 @@
+# apt-get source for google-cloud-sdk
+echo "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -c -s) main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
 # Update local package directory
 apt-get -y update
 
@@ -14,7 +18,8 @@ apt-get -y install \
   make \
   direnv \
   tig \
-  jq
+  jq \
+  google-cloud-sdk
 
 # Install any custom files
 cp -R all/* /
