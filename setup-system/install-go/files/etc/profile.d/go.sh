8c1a08a3710e8f53d2dbb70226231636be80f831
Leigh McCulloch <leigh@mcchouse.com>
2016-12-23 06:13:24 +0000

    Remove the system gopath from GOPATH
    What
    ===
    Remove the system gopath from GOPATH.
    
    Why
    ===
    While some go tools respect a GOPATH with multiple entries, the guru
    tool does not and errors when multiple entries are present. The reason
    there are multiple entries in this setup is because tools like guru are
    installed in the system GOPATH to remove clutter from the user's GOPATH.
    It turns out this is unnecessary. Once the tools are installed there is
    no need to keep them in the GOPATH. The only thing that is necessary is
    to have the bin directory in the PATH, which is already the case on the
    next line.

--- setup-system/install-go/files/etc/profile.d/go.sh
+++ setup-system/install-go/files/etc/profile.d/go.sh
@@ -3,5 +3,5 @@
 export GOROOT=/usr/local/go
 export GOPATH_SYSTEM=/usr/local/gopath
 export GOPATH_USER=$HOME/go
-export GOPATH=$GOPATH_USER:$GOPATH_SYSTEM
+export GOPATH=$GOPATH_USER
 export PATH=$PATH:$GOROOT/bin:$GOPATH_USER/bin:$GOPATH_SYSTEM/bin
