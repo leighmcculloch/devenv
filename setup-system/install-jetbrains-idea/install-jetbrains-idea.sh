062f93327bf7fbe4425591eb7eca445a6a8e63d3
Leigh McCulloch <leigh@mcchouse.com>
2017-01-02 17:15:34 -0800

    Remove IDEA

--- setup-system/install-jetbrains-idea/install-jetbrains-idea.sh
+++
@@ -1,7 +0,0 @@
-apt-get -y install \
-  libxrender1 \
-  libxtst6 \
-  libxi6
-wget -qO - https://download.jetbrains.com/idea/ideaIC-2016.3.1.tar.gz \
-  | tar xz -C /usr/local
-ln -s /usr/local/idea-IC-163.9166.29/bin/idea.sh /usr/local/bin/idea
