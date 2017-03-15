0f5a75ac1dc63d4b7b5c5812a89eb861b9bff33a
Leigh McCulloch <leigh@mcchouse.com>
2017-03-14 23:15:03 -0700

    Add users to rvm group automatically if rvm installed

--- setup-system/install-ruby/install-ruby.sh
+++ setup-system/install-ruby/install-ruby.sh
@@ -1,2 +1,3 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
+echo "\nEXTRA_GROUPS=\"rvm\"\nADD_EXTRA_GROUPS=1" >> /etc/adduser.conf
