aafd35085a6753bffa7d29a4449df9f7a2009f4b
Leigh McCulloch <leigh@mcchouse.com>
2017-10-26 20:54:24 -0700

    Fix the ruby RVM group assignment

--- setup-system/install-ruby/install-ruby.sh
+++ setup-system/install-ruby/install-ruby.sh
@@ -1,3 +1,5 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-echo "\nEXTRA_GROUPS=\"rvm\"\nADD_EXTRA_GROUPS=1" >> /etc/adduser.conf
+echo >> /etc/adduser.conf
+echo EXTRA_GROUPS=rvm >> /etc/adduser.conf
+echo ADD_EXTRA_GROUPS=1 >> /etc/adduser.conf
