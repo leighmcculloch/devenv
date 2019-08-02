83003da660531708cbc485ee248b51b67de2caa1
Leigh McCulloch <leigh@mcchouse.com>
2019-08-02 21:49:58 +0000

    Allow easier docker usage inside devenv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
+# Allow using the docker socket without root.
+sudo chgrp docker /var/run/docker.sock
+
+# Start-up the SSH-Agent so it can be used across all tmux panes/windows.
 eval $(ssh-agent -s)
 
+# Start TMUX for all terminal access.
 tmux -2 new
